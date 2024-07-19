b50aba3f7865cb0300d1fc103d725f96e26349d1
smirnovds1990 <smirnovds1990@gmail.com>
2024-07-19 11:29:55 +0300

    style: add header.html and adjust the styles

--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from . import views
@@ -7,4 +8,14 @@ app_name = 'users'
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
+    path(
+        'logout/',
+        LogoutView.as_view(template_name='users/logout.html'),
+        name='logout'
+    ),
+    path(
+        'login/',
+        LoginView.as_view(template_name='users/login.html'),
+        name='login'
+    ),
 ]
--- weather_app/settings.py
+++ weather_app/settings.py
@@ -125,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_URL = 'users:login'
+
+LOGIN_REDIRECT_URL = 'weather:index'
