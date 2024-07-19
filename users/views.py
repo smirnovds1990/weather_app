a90cf765b1813cb1ffe964ff06e03667f18dfc08
smirnovds1990 <smirnovds1990@gmail.com>
2024-07-19 10:06:02 +0300

    feat: create the weather app and the index page

--- users/views.py
+++ users/views.py
@@ -1,15 +1,10 @@
-from django.http import HttpResponse
 from django.views.generic import CreateView
 from django.urls import reverse_lazy
 
 from .forms import CreationForm
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
-
-
 class SignUpView(CreateView):
     form_class = CreationForm
-    success_url = reverse_lazy('index')
+    success_url = reverse_lazy('weather:index')
     template_name = 'users/signup.html'
--- weather_app/urls.py
+++ weather_app/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+
 urlpatterns = [
     path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
+    path("", include('weather.urls')),
 ]
