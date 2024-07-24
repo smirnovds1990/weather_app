60319d3958beab12ff7cf729582953258199a31d
smirnovds1990 <smirnovds1990@gmail.com>
2024-07-24 13:05:35 +0300

    feat: add City model and make migrations

--- weather/models.py
+++ weather/models.py
@@ -1,3 +1,24 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class City(models.Model):
+    city_title = models.CharField(
+        verbose_name='Название города',
+        blank=False,
+        null=False,
+        max_length=100
+    )
+    user = models.ManyToManyField(
+        User,
+        related_name='cities',
+        verbose_name='Пользователь',
+        blank=False
+    )
+    request_date = models.DateTimeField(
+        verbose_name='Дата запроса',
+        auto_now_add=True
+    )
